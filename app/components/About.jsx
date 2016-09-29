d8a715ff9902c1fe42bb1bb08a5faec11d038fc0
Joe Roland <joroland@gmail.com>
2016-09-28 20:35:18 -0400

    Style about page & and add resource links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,20 @@ const React = require('react');
 const About = (props) => {
   return(
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page!</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a simple weather application built with React.<br/>
+        The code for this app is available on <a href="https://github.com/JPRoland/ReactWeatherApp">Github</a>
+      </p>
+      <p>Some of the tools used to build this app:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a>
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a>
+        </li>
+      </ul>
     </div>
   )
 };
--- public/bundle.js
+++ public/bundle.js
@@ -28782,23 +28782,57 @@
 /* 270 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(8);
 
 	var About = function About(props) {
 	  return React.createElement(
-	    'div',
+	    "div",
 	    null,
 	    React.createElement(
-	      'h3',
-	      null,
-	      'About'
+	      "h1",
+	      { className: "text-center" },
+	      "About"
 	    ),
 	    React.createElement(
-	      'p',
+	      "p",
 	      null,
-	      'Welcome to the about page!'
+	      "This is a simple weather application built with React.",
+	      React.createElement("br", null),
+	      "The code for this app is available on ",
+	      React.createElement(
+	        "a",
+	        { href: "https://github.com/JPRoland/ReactWeatherApp" },
+	        "Github"
+	      )
+	    ),
+	    React.createElement(
+	      "p",
+	      null,
+	      "Some of the tools used to build this app:"
+	    ),
+	    React.createElement(
+	      "ul",
+	      null,
+	      React.createElement(
+	        "li",
+	        null,
+	        React.createElement(
+	          "a",
+	          { href: "https://facebook.github.io/react" },
+	          "React"
+	        )
+	      ),
+	      React.createElement(
+	        "li",
+	        null,
+	        React.createElement(
+	          "a",
+	          { href: "http://openweathermap.org" },
+	          "Open Weather Map"
+	        )
+	      )
 	    )
 	  );
 	};
